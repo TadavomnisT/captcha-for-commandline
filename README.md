8f8504c960b085af1f1d1b1cb4cb2fd133eb7814
0x6D7366 <MsfNemati1386@gmail.com>
2023-09-15 19:35:56 +0330

    Update ascii2art_based_captcha

--- README.md
+++ README.md
@@ -1,16 +1,19 @@
 # captcha-for-commandline
+
 Here you can find a few commandline-based CAPTCHAs.
 
 ## Usage
 
-```
+```bash
 php ascii2art_based_captcha.php
 ```
 
 ## Requirements
+
 * [jp2a](https://github.com/cslarsen/jp2a) (`sudo apt install jp2a` on debian)
 * PHP (`sudo apt install php` on debian)
 * PHP-gd (`sudo apt install php-gd` on debian)
 
 ## ScreenShot
-![ascii2art_based_captcha,php result](https://raw.githubusercontent.com/TadavomnisT/captcha-for-commandline/main/ascii2art_based_captcha_result.jpg)
+
+![ascii2art_based_captcha,php result](ascii2art_based_captcha_result.jpg)
--- ascii2art_based_captcha.php
+++ ascii2art_based_captcha.php
@@ -1,63 +1,64 @@
 <?php
 
-if (rand(0,100) > 50)
-{
-    $sgn = true;
-    $a = rand (0,500);
-    $b = rand (0,500);
-}
-else
-{
-    $sgn = false;
-    $a = rand (500,1000);
-    $b = rand (0,500);
+function apply_wave_effect($image, $width, $height) {  
+  $x_period = 20;
+  $y_period = 20;
+  $y_amplitude = 15;
+  $x_amplitude = 15;
+  $xp = $x_period * rand(1, 3);
+  $k = rand(0, 100);
+  for ($a = 0; $a < $width; $a++)
+    imagecopy($image, $image, $a-1, sin($k + $a / $xp) * $x_amplitude, $a, 0, 1, $height);
+  
+  $yp = $y_period * rand(1, 2);
+  $k = rand(0, 100);
+  
+  for ($a = 0; $a < $height; $a++)
+    imagecopy($image, $image, sin($k + $a / $yp) * $y_amplitude, $a - 1, 0, $a, $width, 1);
+  
+  return $image;
 }
 
-$answer = ($sgn) ? ($a+$b) : ($a-$b) ;
+$sgn = !rand(0, 1);
+$a = $sgn ? rand(0, 500) : rand(500, 999);
+$b = rand(0, 500);
 
-$string = $a . (($sgn) ? "+" : "-") . $b;
+$string = $a . ($sgn ? '+' : '-') . $b;
+$answer = $sgn ? ($a + $b) : ($a - $b);
 
 $width = 600;
 $height = 300;
-$im = imagecreatetruecolor($width, $height);
+$img = imagecreatetruecolor($width, $height);
 
-$background_color = imagecolorallocate($im, 255, 255, 255);
-$text_color = imagecolorallocate($im, 0, 0, 0);
+$background_color = imagecolorallocate($img, 255, 255, 255);
+$text_color = imagecolorallocate($img, 0, 0, 0);
 
-imagefilledrectangle($im, 0, 0, $width - 1, $height - 1, $background_color);
+imagefilledrectangle($img, 0, 0, $width - 1, $height - 1, $background_color);
 
-$font_file = (rand(0,100) > 50) ? "./zxx-noise.ttf" : "./zxx-xed.ttf" ;
-$font_size = 100;
-$y = 180;
+$font_files = ['zxx-noise.ttf', 'zxx-xed.ttf'];
+$font_file = './' . $font_files[array_rand($font_files, 1)];
+$font_size = 90;
 
-imagettftext($im, $font_size, 0, 10, $y, $text_color, $font_file, $string);
+$textBoundingBox = imagettfbbox($font_size, 0, $font_file, $string);
+$text_width = $textBoundingBox[2] - $textBoundingBox[0];
+$text_height = $textBoundingBox[7] - $textBoundingBox[1];
+$text_x = ($width - $text_width) / 2;
+$text_y = ($height - $text_height) / 2;
 
-$im = applyWave($im, imagesx($im), imagesy($im));
+imagettftext($img, $font_size, 0, $text_x, $text_y, $text_color, $font_file, $string);
+$img = apply_wave_effect($img, imagesx($img), imagesy($img));
 
 ob_start();
-header('Content-type: image/png');
-imagepng($im);
-imagedestroy($im);
-file_put_contents( $fname = md5( microtime(true) ) , ob_get_clean() );
+imagepng($img);
+imagedestroy($img);
 
-echo shell_exec( "jp2a " . $fname );
-unlink( $fname );
+file_put_contents($file_name = md5(microtime(true)), ob_get_clean());
 
-function applyWave($image, $width, $height)
-{    
-    $x_period = 20;
-    $y_period = 20;
-    $y_amplitude = 15;
-    $x_amplitude = 15;
-    $xp = $x_period*rand(1,3);
-    $k = rand(0,100);
-    for ($a = 0; $a<$width; $a++)
-        imagecopy($image, $image, $a-1, sin($k+$a/$xp)*$x_amplitude, $a, 0, 1, $height);
-    $yp = $y_period*rand(1,2);
-    $k = rand(0,100);
-    for ($a = 0; $a<$height; $a++)
-        imagecopy($image, $image, sin($k+$a/$yp)*$y_amplitude, $a-1, 0, $a, $width, 1);
-    return $image;
-}
+$ascii_art = shell_exec("jp2a $file_name");
+unlink($file_name);
 
-?>
+echo "\n$ascii_art\n";
+
+$input = readline('Say answer: ');
+
+echo "\n " . ['Oops.. :(', 'Bravo.. :)']["$input" === "$answer"] . "\n\n";
