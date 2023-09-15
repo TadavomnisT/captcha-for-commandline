d918f3521f164c2abbb15dd1eb30ef10fd56d850
0x6D7366 <MsfNemati1386@gmail.com>
2023-09-15 20:09:36 +0330

    Update

--- ascii2art_based_captcha.php
+++ ascii2art_based_captcha.php
@@ -1,64 +1,63 @@
 <?php
 
-function apply_wave_effect($image, $width, $height) {  
-  $x_period = 20;
-  $y_period = 20;
-  $y_amplitude = 15;
-  $x_amplitude = 15;
-  $xp = $x_period * rand(1, 3);
-  $k = rand(0, 100);
-  for ($a = 0; $a < $width; $a++)
-    imagecopy($image, $image, $a-1, sin($k + $a / $xp) * $x_amplitude, $a, 0, 1, $height);
-  
-  $yp = $y_period * rand(1, 2);
-  $k = rand(0, 100);
-  
-  for ($a = 0; $a < $height; $a++)
-    imagecopy($image, $image, sin($k + $a / $yp) * $y_amplitude, $a - 1, 0, $a, $width, 1);
-  
-  return $image;
+if (rand(0,100) > 50)
+{
+    $sgn = true;
+    $a = rand (0,500);
+    $b = rand (0,500);
+}
+else
+{
+    $sgn = false;
+    $a = rand (500,1000);
+    $b = rand (0,500);
 }
 
-$sgn = !rand(0, 1);
-$a = $sgn ? rand(0, 500) : rand(500, 999);
-$b = rand(0, 500);
+$answer = ($sgn) ? ($a+$b) : ($a-$b) ;
 
-$string = $a . ($sgn ? '+' : '-') . $b;
-$answer = $sgn ? ($a + $b) : ($a - $b);
+$string = $a . (($sgn) ? "+" : "-") . $b;
 
 $width = 600;
 $height = 300;
-$img = imagecreatetruecolor($width, $height);
+$im = imagecreatetruecolor($width, $height);
 
-$background_color = imagecolorallocate($img, 255, 255, 255);
-$text_color = imagecolorallocate($img, 0, 0, 0);
+$background_color = imagecolorallocate($im, 255, 255, 255);
+$text_color = imagecolorallocate($im, 0, 0, 0);
 
-imagefilledrectangle($img, 0, 0, $width - 1, $height - 1, $background_color);
+imagefilledrectangle($im, 0, 0, $width - 1, $height - 1, $background_color);
 
-$font_files = ['zxx-noise.ttf', 'zxx-xed.ttf'];
-$font_file = './' . $font_files[array_rand($font_files, 1)];
-$font_size = 90;
+$font_file = (rand(0,100) > 50) ? "./zxx-noise.ttf" : "./zxx-xed.ttf" ;
+$font_size = 100;
+$y = 180;
 
-$textBoundingBox = imagettfbbox($font_size, 0, $font_file, $string);
-$text_width = $textBoundingBox[2] - $textBoundingBox[0];
-$text_height = $textBoundingBox[7] - $textBoundingBox[1];
-$text_x = ($width - $text_width) / 2;
-$text_y = ($height - $text_height) / 2;
+imagettftext($im, $font_size, 0, 10, $y, $text_color, $font_file, $string);
 
-imagettftext($img, $font_size, 0, $text_x, $text_y, $text_color, $font_file, $string);
-$img = apply_wave_effect($img, imagesx($img), imagesy($img));
+$im = applyWave($im, imagesx($im), imagesy($im));
 
 ob_start();
-imagepng($img);
-imagedestroy($img);
+header('Content-type: image/png');
+imagepng($im);
+imagedestroy($im);
+file_put_contents( $fname = md5( microtime(true) ) , ob_get_clean() );
 
-file_put_contents($file_name = md5(microtime(true)), ob_get_clean());
+echo shell_exec( "jp2a " . $fname );
+unlink( $fname );
 
-$ascii_art = shell_exec("jp2a $file_name");
-unlink($file_name);
+function applyWave($image, $width, $height)
+{    
+    $x_period = 20;
+    $y_period = 20;
+    $y_amplitude = 15;
+    $x_amplitude = 15;
+    $xp = $x_period*rand(1,3);
+    $k = rand(0,100);
+    for ($a = 0; $a<$width; $a++)
+        imagecopy($image, $image, $a-1, sin($k+$a/$xp)*$x_amplitude, $a, 0, 1, $height);
+    $yp = $y_period*rand(1,2);
+    $k = rand(0,100);
+    for ($a = 0; $a<$height; $a++)
+        imagecopy($image, $image, sin($k+$a/$yp)*$y_amplitude, $a-1, 0, $a, $width, 1);
+    return $image;
+}
 
-echo "\n$ascii_art\n";
-
-$input = readline('Say answer: ');
-
-echo "\n " . ['Oops.. :(', 'Bravo.. :)']["$input" === "$answer"] . "\n\n";
+?>
